88515a05f37a0c3369dbb085aaadc26809373299
Rammohan4 <crammohan55@gmail.com>
2024-07-04 12:32:59 +0530

    :Base Class

---
+++ src/test/java/org/base/Classs.java
@@ -0,0 +1,13 @@
+package org.base;
+
+public class Classs extends BaseClass{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		browserLaunch();
+		urlLaunch("https://www.flipkart.com/");
+		getcurrenturl();
+		
+	}
+	
+}
